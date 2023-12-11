0c2349f2d621f3a2337e66424041c3b4d7b71100
Muayid Macs <moahfox@gmail.com>
2023-12-11 20:00:06 +0400

    Add remixicon dependency and update index.blade.php

---
+++ resources/views/components/links.blade.php
@@ -0,0 +1,4 @@
+{{-- Links --}}
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/custom/styles.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/remixicon/remixicon.css') }}">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+    <div id="form">
+        <h1>LinkDrop <span class="icon"><i class="ri-wifi-line"></i></span></h1>
+        <p>Use this one-time link to upload your file. Once uploaded, the file will be automatically deleted.</p>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BunnyCDN Uploader</title>
-</head>
 
-<body>
-    <h1>BunnyCDN Uploader</h1>
-    <p>Upload a file to BunnyCDN</p>
+        <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <label for="">Upload A Random File</label>
+            <input class="custom-file-input" name="file" required type="file">
+            <button type="submit">Submit</button>
+        </form>
 
-    @if (session()->has('success'))
-        <p style="color: green">{{ session()->get('success') }}</p>
-    @endif
-    @if (session()->has('error'))
-        <p style="color: red">{{ session()->get('error') }}</p>
-    @endif
-    <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
-        @csrf
-        <label for="">Upload A Random File</label>
-        <input name="file" required type="file">
-        <button type="submit">Submit</button>
-    </form>
-</body>
-
-</html>
+        <p>
+            @if (session()->has('success'))
+                <p style="color: rgb(130, 130, 130)">{{ session()->get('success') }}</p>
+            @endif
+            @if (session()->has('error'))
+                <p style="color: red">{{ session()->get('error') }}</p>
+            @endif
+        </p>
+    </div>
+</x-layout>
