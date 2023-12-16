3be304827e78ce25742b4b35ee8caf1bb6dd5c2a
Muayid Ahmed <moahfox@gmail.com>
2023-12-16 17:02:51 +0400

    Refactor form input styling and file upload button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,14 @@
 
         <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
             @csrf
-            <label for="">Upload A Random File</label>
-            <input class="custom-file-input" name="file" required type="file">
+            <label for="file-upload" class="custom-file-upload">
+                <i class="fa fa-cloud-upload"></i> Choose File
+            </label>
+            <input id="file-upload" name="file" type="file" style="display:none;" onchange="updateFileName(this)">
+            <span id="file-name"></span>
             <button type="submit">Upload</button>
         </form>
+
         <p>
             @if (session()->has('success'))
                 <p style="color: rgb(130, 130, 130)">{{ session()->get('success') }}</p>
@@ -19,4 +23,12 @@
             @endif
         </p>
     </div>
+
+    <script>
+        function updateFileName(input) {
+            var fileName = input.value.split('\\').pop();
+            document.getElementById('file-name').textContent = fileName;
+        }
+    </script>
+
 </x-layout>
